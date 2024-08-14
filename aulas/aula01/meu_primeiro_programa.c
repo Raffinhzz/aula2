63683b4fbea1f2a73840c7c1f73e32e67a7aa499
Raffinhzz <amorimr4fael@gmail.com>
2024-08-14 13:51:55 +0000

    Codigos da aula 1

---
+++ aulas/aula01/meu_primeiro_programa.c
@@ -0,0 +1,23 @@
+/*
+Construa um programa que calcule a soma de dois numeros inteiros
+*/
+ #include <stdio.h>
+#include <math.h>
+#define PI 3.14159265358979323846
+
+
+ int main()  {
+   int a, b, soma;
+   printf("Digite o primeiro numero: ");
+   scanf("%d", &a);
+   printf("Digite o segundo numero: ");
+   scanf("%d", &b);
+   soma = a + b;
+   printf("A soma de %d e %d eh %d\n", a, b, soma);
+   printf("Seno de 0 %f\n", sin(0));
+   return 0;
+ }
+
+
+
+
